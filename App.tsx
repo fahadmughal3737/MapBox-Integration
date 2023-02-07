0ceed9da3981f98d67eb0afa9456ac4352f50422
Fahad Mughal <fahadmughal3737@gmail.com>
2023-02-07 17:25:45 +0500

    2 graphs

--- App.tsx
+++ App.tsx
@@ -3,16 +3,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {World} from './src/screens/map/worldMap';
 const Stack = createNativeStackNavigator();
-import MapboxGL from '@rnmapbox/maps';
+// import MapboxGL from '@rnmapbox/maps';
 const App = () => {
-  useEffect(() => {
-    MapboxGL.setConnected(true);
-    MapboxGL.setWellKnownTileServer('Mapbox');
+  // useEffect(() => {
+  //   MapboxGL.setConnected(true);
+  //   MapboxGL.setWellKnownTileServer('Mapbox');
 
-    MapboxGL.setAccessToken(
-      'pk.eyJ1IjoiZmFoYWRtdWdoYWwzNzM3IiwiYSI6ImNsZHN2NDVkdTIwNHo0MW4wcnVhZHJscGMifQ.zY1a69BlrCFXTgwDCug1vw',
-    );
-  }, []);
+  //   MapboxGL.setAccessToken(
+  //     'pk.eyJ1IjoiZmFoYWRtdWdoYWwzNzM3IiwiYSI6ImNsZHN2NDVkdTIwNHo0MW4wcnVhZHJscGMifQ.zY1a69BlrCFXTgwDCug1vw',
+  //   );
+  // }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- src/screens/map/worldMap.tsx
+++ src/screens/map/worldMap.tsx
@@ -1,11 +1,116 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, ActivityIndicator} from 'react-native';
 import {styles} from './style';
-// import WorldMap from 'react-native-world-map-svg';
-import {worldmap} from '../../assets/svg/worldmap';
-import MapboxGL from '@rnmapbox/maps';
+import {BarChart} from 'react-native-gifted-charts';
+import {dataSet} from '../../services/dataset/dataset';
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from 'react-native-responsive-screen';
+const lookup = require('country-code-lookup');
 
 export const World = () => {
+  const [load, setLoad] = useState(true);
+  const [barData, setBarData] = useState<any>([]);
+  const [Data, setData] = useState<any>([]);
+
+  useEffect(() => {
+    dataSet.map((data: any, index: number) => {
+      let _country = lookup.byCountry(data['Country or Area']);
+      console.log('__', _country);
+      let _obj = {
+        value: parseInt(data['Internet Users']),
+        // label: _country.fips,
+        label: LabelRender(_country?.iso3),
+      };
+      barData.push(_obj);
+    });
+    dataSet.map((data: any, index: number) => {
+      let _country = lookup.byCountry(data['Country or Area']);
+      console.log('__', _country);
+      let _obj = {
+        value: parseInt(data['Internet Users']),
+        // label: _country.fips,
+        label: LabelHorizontal(_country?.iso3),
+      };
+      Data.push(_obj);
+    });
+    if (barData.length > 0) {
+      console.log('inside IF ');
+      setLoad(false);
+    }
+  }, []);
+  const LabelHorizontal = (text: string) => {
+    return (
+      <View
+        style={{
+          // width: widthPercentageToDP('6.7'),
+          width: widthPercentageToDP('6.7'),
+          // marginLeft: widthPercentageToDP('5'),
+
+          alignItems: 'flex-end',
+        }}>
+        <Text
+          style={{
+            color: 'black',
+            // backgroundColor: 'red',
+            fontWeight: 'bold',
+          }}>
+          {text}
+        </Text>
+      </View>
+    );
+  };
+  const LabelRender = (text: string) => {
+    console.log('test chk', text);
+    let _renderArr = [];
+    _renderArr.push(text?.substring(0, 1));
+    _renderArr.push(text?.substring(1, 2));
+    _renderArr.push(text?.substring(2, 3));
+    console.log('ren', _renderArr);
+    return (
+      <View
+        style={{
+          // width: widthPercentageToDP('6.7'),
+          width: widthPercentageToDP('2.9'),
+          // marginLeft: widthPercentageToDP('5'),
+
+          alignItems: 'flex-end',
+        }}>
+        {/* <Text
+          numberOfLines={1}
+          adjustsFontSizeToFit={true}
+          style={{
+            color: 'black',
+            // backgroundColor: 'red',
+            fontWeight: '400',
+            fontSize: 8,
+          }}>
+          {text}
+  
+        </Text> */}
+        <View style={{marginTop: 15, top: 10}}>
+          {_renderArr.map((data: string, index: number) => {
+            return (
+              <Text
+                numberOfLines={1}
+                adjustsFontSizeToFit={true}
+                style={{
+                  color: 'black',
+
+                  // backgroundColor: 'red',
+
+                  fontWeight: '400',
+                  fontSize: 8,
+                }}>
+                {data}
+              </Text>
+            );
+          })}
+        </View>
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
       <View
@@ -30,35 +135,59 @@ export const World = () => {
             <View style={styles.dataBox}></View>
             <View style={styles.dataBox}></View>
             <View style={styles.dataBox}></View>
-            {/* {values.map((data: any, index: number) => {
-              return (
-                <View style={styles.dataBox}>
-                  <Text style={{fontSize: 16, color: 'black'}}>
-                    {'Country: ' + data['Country or Area']}
-                  </Text>
-                  <Text style={{fontSize: 16, color: 'black'}}>
-                    {'Users: ' + data['Internet Users']}
-                  </Text>
-                </View>
-              );
-            })} */}
           </View>
         </View>
       </View>
       <View style={{flex: 0.9}}>
-        {/* <WorldMap  /> */}
-        {/* <WorldMap
-          color="red"
-          title="Top 10 Populous Countries"
-          value-suffix="people"
-          size="lg"
-          data={data}
-        /> */}
-        {/* <WorldMap data={data} title="aaasd" />
-         */}
-        {/* <WorldMap selected={selected} onSelect={onSelect} /> */}
-        {/* <WorldMap  /> */}
-        <MapboxGL.MapView style={{flex: 1}} />
+        {load ? (
+          <View>
+            <ActivityIndicator size={'large'} />
+          </View>
+        ) : (
+          <>
+            <View
+              style={{
+                flex: 0.5,
+                justifyContent: 'center',
+              }}>
+              <BarChart
+                spacing={10}
+                initialSpacing={20}
+                frontColor={'#177AD5'}
+                barWidth={8}
+                data={barData}
+                // labelWidth={40}
+                width={widthPercentageToDP(80)}
+                disableScroll={true}
+                height={heightPercentageToDP('25')}
+                noOfSections={5}
+                xAxisLabelTextStyle={{}}
+                yAxisLabelSuffix="k"
+                xAxisIndicesWidth={200}
+              />
+            </View>
+            <View
+              style={{
+                flex: 0.5,
+                justifyContent: 'center',
+              }}>
+              <BarChart
+                spacing={10}
+                initialSpacing={20}
+                frontColor={'#177AD5'}
+                barWidth={25}
+                data={Data}
+                // labelWidth={40}
+                width={widthPercentageToDP(80)}
+                // disableScroll={true}
+                height={heightPercentageToDP('25')}
+                noOfSections={5}
+                xAxisLabelTextStyle={{}}
+                yAxisLabelSuffix="k"
+              />
+            </View>
+          </>
+        )}
       </View>
     </View>
   );
